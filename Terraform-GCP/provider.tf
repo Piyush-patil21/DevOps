a0b54e88edccbec2b5ea959bc6070dc69fd76125
Piyush-patil21 <piyushpatil373@gmail.com>
2025-12-05 18:04:28 +0530

    Edited provider.tf file for GCP

--- Terraform-GCP/provider.tf
+++ Terraform-GCP/provider.tf
@@ -2,12 +2,5 @@ provider "google" {
   project                     = "<project-id>"
   region                      = "asia-south1"
   zone                        = "asia-south1-a"
-<<<<<<< HEAD
-  # impersonate_service_account = "1093939435927-compute@developer.gserviceaccount.com"
-  credentials = "credentials\\key.json"
-}
 
-=======
-  # impersonate_service_account = ""
 }
->>>>>>> d2e3aaca15c98f6411f4c902a50b296360920f2a
